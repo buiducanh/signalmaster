3c9e592b5422c3aa627309964d2ad2580b04d448
Anh Bui <b.ducanh96@gmail.com>
2016-04-12 17:13:08 -0400

    add generate ssl certs to procfile

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ var yetify = require('yetify'),
     sockets = require('./sockets'),
     port = parseInt(process.env.PORT || config.server.port, 10),
     server_handler = function (req, res) {
-        res.writeHead(404);
-        res.end();
+        res.end("welcome to socket.io");
     },
     server = null;
 
